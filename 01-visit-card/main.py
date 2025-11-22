4cd523c1362fbe3e1ad16bb5ad3d36302f20db82
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-11-22 20:09:48 +0600

    01-visit-card

---
+++ 01-visit-card/main.py
@@ -0,0 +1,11 @@
+first_name = "Атай"
+last_name = "Аскаров"
+email = "atai@example.com"
+age = 28
+
+print("========== ТААНЫТМА ==========")
+print(f"Аты:           {first_name}")
+print(f"Фамилиясы:     {last_name}")
+print(f"Email:         {email}")
+print(f"Жашы:          {age}")
+print("================================")
