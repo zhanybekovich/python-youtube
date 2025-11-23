2f445f4107a182a70cc0ba1f8b38bd95ea8cf5ef
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-11-23 21:03:44 +0600

    02-strings

---
+++ 02-strings/main.py
@@ -0,0 +1,29 @@
+# 1-тапшырма
+word = "Салам"
+print(word[0])
+print(word[-1])
+
+print("=" * 20)
+
+# 2-тапшырма
+name = "Али"
+print(f"Салам, {name}!")
+
+print("=" * 20)
+
+# 3-тапшырма
+text = "Мен Pythonду жакшы көрөм"
+print(text.upper())
+print(text.lower())
+
+print("=" * 20)
+
+# 4-тапшырма
+phrase = "ананас"
+print(phrase.count("а"))
+
+print("=" * 20)
+
+# 5-тапшырма
+sentence = "мен pythonду күн сайын окуйм"
+print(sentence.title())
