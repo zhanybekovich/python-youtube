d7944b4776ac2dcd31f9076cd1787cc97e92cf41
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-11-25 18:04:07 +0600

    03-numbers-arithmetics

---
+++ 03-numbers/main.py
@@ -0,0 +1,31 @@
+# 1
+a = 8
+b = 5
+
+area = a * b
+print(f"Тик бурчтуктун аянты: {area}")
+
+# 2
+hours = 5
+seconds = hours * 60 * 60
+print(f"{hours} саатта {seconds} секунд бар.")
+
+# 3
+a = 8
+perimeter = a * 4
+print(f"Квадраттын периметри: {perimeter}")
+
+# 4
+tanya = 10
+mitya = 8
+difference = mitya - tanya
+print(f"Таня менен Митянын ортосунда {abs(difference)} жаш айырма бар.")
+
+# 5
+math = 4
+physics = 5
+english = 3
+
+sum = math + physics + english
+average = sum / 3
+print(f"Орточо балл: {average}")
